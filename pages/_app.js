01a3aacede5260da96a92082c99d73e33e75f327
C Flint <flintc27@gmail.com>
2020-03-07 14:46:37 -0500

    added tailwindcss w/ postcss config. tested that it worked.

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import React from "react";
+import App from "next/app";
+import Link from "next/link";
+import "../css/tailwind.css";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <div>
+        <div>
+          <Link href="/">
+            <a className="text-xl font-bold text-purple-800 px-8 hover:text-purple-400">
+              Home
+            </a>
+          </Link>
+          <Link href="/test">
+            <a className="text-xl font-bold text-purple-800 px-8 hover:text-purple-400">
+              Test
+            </a>
+          </Link>
+        </div>
+        <Component {...pageProps} />
+      </div>
+    );
+  }
+}
+
+export default MyApp;
---
+++ pages/test.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <h1 className="text-5xl font-bold text-purple-500 px-8">test!</h1>
+  </div>
+);
