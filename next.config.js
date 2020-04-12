e702377259eb470d9609a3e87c172fcf193a4dc7
C Flint <flintc27@gmail.com>
2020-04-12 10:55:23 -0400

    added api endpoint to initialize timer.

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    HGE_ENDPOINT: "https://mysterious-hollows-61253.herokuapp.com/v1/graphql",
+  },
+};
---
+++ pages/api/initTimer.js
@@ -0,0 +1,49 @@
+const { json, send } = require("micro");
+const { query } = require("graphqurl");
+
+const HGE_ENDPOINT = process.env.HGE_ENDPOINT;
+
+const MUTATION_SET_TIME_REMAINING = `
+mutation SetTimeRemaing ($roomName: String) {
+  update_game(where: {name: {_eq: $roomName}}, _set: {time_reamaining: 180000}){
+    returning{
+      id
+      name
+      time_reamaining
+    }
+  }
+}
+`;
+
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    send(res, 404);
+  }
+  let payload;
+  try {
+    payload = await json(req);
+  } catch (error) {
+    send(res, 400, { error });
+    return;
+  }
+
+  const {
+    id,
+    event: { op, data },
+    table,
+    trigger,
+  } = payload;
+
+  try {
+    const result = await query({
+      query: MUTATION_SET_TIME_REMAINING,
+      endpoint: HGE_ENDPOINT,
+      variables: {
+        roomName: data.new.name,
+      },
+    });
+    send(res, 200, { result });
+  } catch (error) {
+    send(res, 500, { error });
+  }
+};
